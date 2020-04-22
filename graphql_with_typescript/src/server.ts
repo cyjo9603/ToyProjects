eb8ec8a5de4e7f3d37a381e74337fd05994f50e3
Chanyeong Cho <cyjo9603@naver.com>
2020-04-22 16:37:39 +0900

    [gwt : feat] delete http module

--- graphql_with_typescript/src/server.ts
+++ graphql_with_typescript/src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import depthLimit from 'graphql-depth-limit';
-import { createServer } from 'http';
 import compression from 'compression';
 import cors from 'cors';
 
@@ -29,5 +28,4 @@ app.use(compression());
 
 server.applyMiddleware({ app, path: '/graphql' });
 
-const httpServer = createServer(app);
-httpServer.listen(app.get('port'), () => console.log(`server is running on ${app.get('port')}`));
+app.listen(app.get('port'), () => console.log(`server is running on ${app.get('port')}`));
