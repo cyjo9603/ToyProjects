47726d87f8a0979f34d557259ecc16e2f79c4566
Chanyeong Cho <cyjo9603@naver.com>
2020-04-30 22:05:01 +0900

    [cke : feat] print content

--- react_ckeditor/components/Editor.tsx
+++ react_ckeditor/components/Editor.tsx
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 const Editor = () => {
+  const [content, setContent] = useState('');
+
+  const showContent = () => {
+    console.log(content);
+  };
+
   return (
-    <CKEditor
-      editor={ClassicEditor}
-      data="<p>Hello from CKEditor 5!</p>"
-      onInit={(editor: any) => {
-        // You can store the "editor" and use when it is needed.
-        console.log('Editor is ready to use!', editor);
-      }}
-      onChange={(event: any, editor: any) => {
-        const data = editor.getData();
-        console.log({ event, editor, data });
-      }}
-      onBlur={(event: any, editor: any) => {
-        console.log('Blur.', editor);
-      }}
-      onFocus={(event: any, editor: any) => {
-        console.log('Focus.', editor);
-      }}
-    />
+    <>
+      <CKEditor
+        editor={ClassicEditor}
+        data="<p>Hello from CKEditor 5!</p>"
+        onInit={(editor: any) => {
+          console.log('Editor is ready to use!', editor);
+        }}
+        onChange={(event: any, editor: any) => {
+          setContent(editor.getData());
+        }}
+      />
+      <button onClick={showContent}>GET CONTENT</button>
+    </>
   );
 };
 
