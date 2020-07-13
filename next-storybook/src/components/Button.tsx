637dd81b5c4eb744fcc9fa82bd681a69dec67f47
Chanyeong Cho <cyjo9603@naver.com>
2020-07-13 20:08:17 +0900

    [stb : feat] Set Button Story

--- next-storybook/src/components/Button.tsx
+++ next-storybook/src/components/Button.tsx
@@ -1,29 +1,72 @@
 import React from 'react';
 import styled from 'styled-components';
 
+type ButtonType = 'default' | 'primary' | 'error' | 'dashed';
+
 interface Props {
   text: string;
-  onClick: () => void;
+  type?: ButtonType;
+  onClick?: () => void;
 }
 
-const ButtonWrapper = styled.button`
-  border: 1px solid #000000;
+const ButtonWrapper = styled.button<{ buttonType: ButtonType }>`
+  border: ${({ buttonType }) => getBorder(buttonType)};
   border-radius: 4px;
-  background: rgba(0, 0, 0, 0.5);
+  background-color: ${({ buttonType }) => getBackgroundColor(buttonType)};
   padding: 10px 18px;
   cursor: pointer;
-  color: #fff;
+  color: ${({ buttonType }) => getColor(buttonType)};
   font-size: 16px;
+  transition: opacity 0.3s;
 
   &:focus {
     outline: none;
   }
 
   &:hover {
-    background: rgba(0, 0, 0, 0.4);
+    opacity: 0.7;
   }
 `;
 
-const Button = ({ text, onClick }: Props) => <ButtonWrapper onClick={onClick}>{text}</ButtonWrapper>;
+const Button = ({ text, type = 'default', onClick }: Props) => (
+  <ButtonWrapper onClick={onClick} buttonType={type}>
+    {text}
+  </ButtonWrapper>
+);
+
+function getColor(type: ButtonType) {
+  switch (type) {
+    case 'dashed':
+    case 'default':
+      return '#888888';
+    case 'primary':
+    case 'error':
+      return '#ffffff';
+  }
+}
+
+function getBackgroundColor(type: ButtonType) {
+  switch (type) {
+    case 'dashed':
+    case 'default':
+      return '#ffffff';
+    case 'primary':
+      return '#1890ff';
+    case 'error':
+      return '#ff4d4f';
+  }
+}
+
+function getBorder(type: ButtonType) {
+  switch (type) {
+    case 'default':
+      return '1px solid #cccccc';
+    case 'primary':
+    case 'error':
+      return 'none';
+    case 'dashed':
+      return '1px dashed #cccccc';
+  }
+}
 
 export default Button;
--- next-storybook/src/stories/Button.stories.tsx
+++ next-storybook/src/stories/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { storiesOf, addDecorator } from '@storybook/react';
 import { withKnobs, text } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 
 import Button from '../components/Button';
 
@@ -13,5 +13,23 @@ export default {
 export const Default = () => {
   const content = text('content', 'default');
 
-  return <Button text={content} onClick={() => console.log('test')} />;
+  return <Button text={content} onClick={action('clicked')} />;
+};
+
+export const Primary = () => {
+  const content = text('content', 'primary');
+
+  return <Button text={content} type="primary" onClick={action('clicked')} />;
+};
+
+export const Error = () => {
+  const content = text('content', 'error');
+
+  return <Button text={content} type="error" onClick={action('clicked')} />;
+};
+
+export const Dashed = () => {
+  const content = text('content', 'dashed');
+
+  return <Button text={content} type="dashed" onClick={action('clicked')} />;
 };
