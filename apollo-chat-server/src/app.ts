c9130146e019093d063ce7363599dd0001d59231
chanyeong <cyjo9603@naver.com>
2020-11-15 21:27:58 +0900

    [feat] socket 서버 설정
    - 실시간 채팅 기능을 위한 socket 서버 설정

--- apollo-chat-server/src/app.ts
+++ apollo-chat-server/src/app.ts
@@ -1,27 +1,39 @@
-import { ApolloServer, PubSub } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import express, { Express } from 'express';
 import logger from 'morgan';
+import { createServer, Server } from 'http';
+import { PubSub } from 'graphql-subscriptions';
 
 import schema from './schema';
 
 const GRAPHQL_ENDPOINT = '/graphql' as const;
 
-const pubsub = new PubSub();
-
 class App {
   public app: Express;
-  private server: ApolloServer;
+  public server: Server;
+  private pubsub: PubSub;
+  private apolloServer: ApolloServer;
 
   constructor() {
+    this.pubsub = new PubSub();
     this.app = express();
-    this.server = new ApolloServer({ schema, context: { pubsub } });
+    this.apolloServer = new ApolloServer({
+      schema,
+      context: (ctx) => ({ ...ctx, pubsub: this.pubsub }),
+      playground: true,
+    });
+    this.server = createServer(this.app);
     this.middlewares();
   }
 
   private middlewares() {
     this.app.use(logger('dev'));
-    this.server.applyMiddleware({ app: this.app, path: GRAPHQL_ENDPOINT });
+    this.apolloServer.applyMiddleware({
+      app: this.app,
+      path: GRAPHQL_ENDPOINT,
+    });
+    this.apolloServer.installSubscriptionHandlers(this.server);
   }
 }
 
-export default new App().app;
+export default new App();
--- apollo-chat-server/src/index.ts
+++ apollo-chat-server/src/index.ts
@@ -2,4 +2,4 @@ import app from './app';
 
 const PORT = 4000;
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
