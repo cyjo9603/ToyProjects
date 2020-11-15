c6efa847a837bffe658622056708a9e2cf0d9ba1
chanyeong <cyjo9603@naver.com>
2020-11-15 22:38:46 +0900

    [env] client-react 기본 설정
    - 웹팩 및 바벨, 리액트 설정

---
+++ apollo-chat-client/src/app.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+import Home from './components/Home';
+
+const App: FC = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default App;
---
+++ apollo-chat-client/src/components/Home/Home.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+const Home: FC = () => <div>init</div>;
+
+export default Home;
