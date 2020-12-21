ebeb7ace892e810a3ff85503957277330da231a6
chanyeong <cyjo9603@naver.com>
2020-12-21 22:13:45 +0900

    [chore] passport-local 기본 설정
    - passport-local 기본 설정

--- apollo-passport/src/app.ts
+++ apollo-passport/src/app.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import logger from 'morgan';
 
+import passportInit from '@auth/passport';
 import schema from './schema';
 
 const GRAPHQL_ENDPOINT = '/graphql';
@@ -12,12 +13,16 @@ class App {
 
   constructor() {
     this.app = express();
-    this.apollo = new ApolloServer({ schema });
+    this.apollo = new ApolloServer({
+      schema,
+      context: ({ req, res }) => ({ req, res }),
+    });
     this.middlewares();
   }
 
   middlewares() {
     this.app.use(logger('dev'));
+    passportInit();
     this.apollo.applyMiddleware({
       app: this.app,
       path: GRAPHQL_ENDPOINT,
---
+++ apollo-passport/src/auth/passport.ts
@@ -0,0 +1,19 @@
+import passport from 'passport';
+import { Strategy, VerifyFunction } from 'passport-local';
+
+const DUMMY_USER_ID = 'id';
+const DUMMY_PASSWORD = 'pw';
+
+const passportConfig = { usernameField: 'userId', passwordField: 'password' };
+
+const passportVerify: VerifyFunction = (userId, password, done) => {
+  if (userId === DUMMY_USER_ID && password === DUMMY_PASSWORD) {
+    done(null, { result: true });
+    return;
+  }
+  done(null, false, { message: 'fail' });
+};
+
+export default () => {
+  passport.use('local', new Strategy(passportConfig, passportVerify));
+};
