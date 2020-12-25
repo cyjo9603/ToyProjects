3e5d7f4b19ec542a20f7471dced0a7944000aa6b
chanyeong <cyjo9603@naver.com>
2020-12-25 10:16:23 +0900

    [feat] decorator 학습
    - dacorator 학습

--- nomad-nest/src/movies/movies.controller.ts
+++ nomad-nest/src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,14 +16,19 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
+  @Get('search')
+  search(@Query('year') seachingYear: string) {
+    return `We are searching for a movie made after: ${seachingYear}`;
+  }
+
   @Get('/:id')
   getOne(@Param('id') movieId: string) {
     return `This will return on movie with the id: ${movieId}`;
   }
 
   @Post()
-  create() {
-    return 'This wil create a movie';
+  create(@Body() movieData) {
+    return movieData;
   }
 
   @Delete('/:id')
@@ -23,7 +37,10 @@ export class MoviesController {
   }
 
   @Patch('/:id')
-  patch(@Param('id') movieId: string) {
-    return `This is will patch a movie with the id: ${movieId}`;
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
