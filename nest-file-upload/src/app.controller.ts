9fad6ddca86b1ff16c6ed851d6380d7a04296e05
chanyeong <cyjo9603@naver.com>
2021-01-18 15:24:57 +0900

    [nest-file-upload] Chore : File-uploader 초기 라우팅 구현
    - 파일 업로드를 받을 수 있는 라우팅만 구현
    - 아직 s3와 연동하지 않음

--- nest-file-upload/src/app.controller.ts
+++ nest-file-upload/src/app.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +10,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('image'))
+  uploadImage(@UploadedFile() file) {
+    console.log(file);
   }
 }
