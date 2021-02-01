b9247a7cf508f095bb3929ad3a597726f01dc86c
chanyeong <cyjo9603@naver.com>
2021-02-01 17:08:09 +0900

    [react-hook-form-validate] Feat: 폼 필수값 및 활성화 적용
    - 폼의 값에 따라 필수 값 및 활성화 되는 필드가 변경되도록 적용

--- react-hook-form-validate/src/components/AntdForm.tsx
+++ react-hook-form-validate/src/components/AntdForm.tsx
@@ -19,7 +19,12 @@ const tailLayout = {
 };
 
 const AntdForm: FC = () => {
-  const { handleSubmit, control } = useForm();
+  const { handleSubmit, control, errors, watch } = useForm();
+
+  const watchOptionalA = watch('optional-A');
+  const watchOptionalB = watch('optional-B');
+  const watchRequiredE = watch('required-E');
+  const watchRequiredF = watch('required-F');
 
   const onSubmit = (values: any) => {
     console.log(values);
@@ -27,20 +32,32 @@ const AntdForm: FC = () => {
 
   return (
     <Form {...formItemLayout} onFinish={handleSubmit(onSubmit)}>
-      <Form.Item label="필수값-A">
+      <Form.Item
+        label="필수값-A"
+        validateStatus={errors['required-A'] ? 'error' : 'success'}
+        help={errors['required-A'] && '입력해주세요'}
+        required
+      >
         <Controller
           name="required-A"
           control={control}
+          rules={{ required: true }}
           render={({ onChange, value }) => (
             <Input placeholder="필수값-A" onChange={onChange} value={value} />
           )}
         />
       </Form.Item>
 
-      <Form.Item label="필수값-B">
+      <Form.Item
+        label="필수값-B"
+        validateStatus={errors['required-B'] ? 'error' : 'success'}
+        help={errors['required-B'] && '입력해주세요'}
+        required
+      >
         <Controller
           name="required-B"
           control={control}
+          rules={{ required: true }}
           render={({ onChange, value }) => (
             <Input
               placeholder="필수값-B"
@@ -52,10 +69,16 @@ const AntdForm: FC = () => {
         />
       </Form.Item>
 
-      <Form.Item label="필수값-C">
+      <Form.Item
+        label="필수값-C"
+        validateStatus={errors['required-C'] ? 'error' : 'success'}
+        help={errors['required-C'] && '입력해주세요'}
+        required
+      >
         <Controller
           name="required-C"
           control={control}
+          rules={{ required: true }}
           render={({ onChange, value }) => (
             <Input placeholder="필수값-C" onChange={onChange} value={value} />
           )}
@@ -72,10 +95,16 @@ const AntdForm: FC = () => {
         />
       </Form.Item>
 
-      <Form.Item label="선택값-B">
+      <Form.Item
+        label="선택값-B"
+        validateStatus={errors['optional-B'] ? 'error' : 'success'}
+        help={errors['optional-B'] && '입력해주세요'}
+        required={Boolean(watchOptionalA)}
+      >
         <Controller
           name="optional-B"
           control={control}
+          rules={{ required: Boolean(watchOptionalA) }}
           render={({ onChange, value }) => (
             <Input
               placeholder="선택값-B (선택값-A가 입력된 경우 필수)"
@@ -86,24 +115,38 @@ const AntdForm: FC = () => {
         />
       </Form.Item>
 
-      <Form.Item label="선택값-C">
-        <Controller
-          name="optional-C"
-          control={control}
-          render={({ onChange, value }) => (
-            <Input
-              placeholder="선택값-C (선택값-B가 입력된 경우 활성화 및 필수)"
-              onChange={onChange}
-              value={value}
-            />
-          )}
-        />
-      </Form.Item>
+      {watchOptionalB && (
+        <Form.Item
+          label="선택값-C"
+          validateStatus={errors['optional-C'] ? 'error' : 'success'}
+          help={errors['optional-C'] && '입력해주세요'}
+          required={true}
+        >
+          <Controller
+            name="optional-C"
+            control={control}
+            rules={{ required: true }}
+            render={({ onChange, value }) => (
+              <Input
+                placeholder="선택값-C (선택값-B가 입력된 경우 활성화 및 필수)"
+                onChange={onChange}
+                value={value}
+              />
+            )}
+          />
+        </Form.Item>
+      )}
 
-      <Form.Item label="필수값-D">
+      <Form.Item
+        label="필수값-D"
+        validateStatus={errors['required-D'] ? 'error' : 'success'}
+        help={errors['required-D'] && '입력해주세요'}
+        required
+      >
         <Controller
           name="required-D"
           control={control}
+          rules={{ required: true }}
           render={({ onChange, value }) => (
             <DatePicker style={{ width: '100%' }} onChange={onChange} value={value} />
           )}
@@ -120,10 +163,16 @@ const AntdForm: FC = () => {
         />
       </Form.Item>
 
-      <Form.Item label="필수값-E">
+      <Form.Item
+        label="필수값-E"
+        validateStatus={errors['required-E'] ? 'error' : 'success'}
+        help={errors['required-E'] && '입력해주세요'}
+        required
+      >
         <Controller
           name="required-E"
           control={control}
+          rules={{ required: true }}
           render={({ onChange, value }) => (
             <Select onChange={onChange} value={value}>
               <Select.Option value="1">Option 1</Select.Option>
@@ -134,10 +183,16 @@ const AntdForm: FC = () => {
         />
       </Form.Item>
 
-      <Form.Item label="필수값-F">
+      <Form.Item
+        label="필수값-F"
+        validateStatus={errors['required-F'] ? 'error' : 'success'}
+        help={errors['required-F'] && '입력해주세요'}
+        required
+      >
         <Controller
           name="required-F"
           control={control}
+          rules={{ required: true }}
           render={({ onChange, value }) => (
             <Cascader
               onChange={onChange}
@@ -152,19 +207,27 @@ const AntdForm: FC = () => {
         />
       </Form.Item>
 
-      <Form.Item label="선택값-E">
-        <Controller
-          name="optional-E"
-          control={control}
-          render={({ onChange, value }) => (
-            <Input
-              placeholder="선택값-E (필수값-E가 2, 필수값-F가 xx1인 경우 활성화 및 필수)"
-              onChange={onChange}
-              value={value}
-            />
-          )}
-        />
-      </Form.Item>
+      {watchRequiredE === '2' && watchRequiredF?.[0] === 'xx1' && (
+        <Form.Item
+          label="선택값-E"
+          validateStatus={errors['optional-E'] ? 'error' : 'success'}
+          help={errors['optional-E'] && '입력해주세요'}
+          required
+        >
+          <Controller
+            name="optional-E"
+            control={control}
+            rules={{ required: true }}
+            render={({ onChange, value }) => (
+              <Input
+                placeholder="선택값-E (필수값-E가 2, 필수값-F가 xx1인 경우 활성화 및 필수)"
+                onChange={onChange}
+                value={value}
+              />
+            )}
+          />
+        </Form.Item>
+      )}
       <Form.Item {...tailLayout}>
         <Button type="primary" htmlType="submit">
           입력
