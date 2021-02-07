69cb4a11462a016766c643123c76c322ac8b9e73
Chanyeong <cyjo9603@naver.com>
2021-02-07 17:43:02 +0900

    [nest-file-upload] Feat : NestJS 기본 query 작성
    - 기본 query가 없으면 서버가 시작되지 않음

--- nest-file-upload/src/upload/upload.resolver.ts
+++ nest-file-upload/src/upload/upload.resolver.ts
@@ -1,4 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query } from '@nestjs/graphql';
+
+import { CoreResponse } from './dto/core.dto';
+import { UploadService } from './upload.service';
 
 @Resolver()
-export class UploadResolver {}
+export class UploadResolver {
+  constructor(private uploadService: UploadService) {}
+
+  @Query((returns) => CoreResponse)
+  hello() {
+    return { result: true };
+  }
+}
